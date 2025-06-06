745adc803c6e6b38b8f581914f30a65a33245ee3
Soumya <soumyajagannathojha7@gmail.com>
2025-06-06 10:10:06 +0530

    adjust margin, padding

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -18,7 +18,7 @@ export default function WeatherApp() {
   };
 
   return (
-    <div style={{ textAlign: "center" }}>
+    <div style={{ textAlign: "center" }} className="container">
       <h2>Weather App by Soumya</h2>
       <SearchBox updateInfo={updateInfo} />
       <InfoBox info={weatherInfo} />
